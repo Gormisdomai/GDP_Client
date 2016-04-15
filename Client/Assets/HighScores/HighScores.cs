362068546c228f21281abddf06b0f26c7b72573e
georgefst <georgefsthomas@gmail.com>
2016-04-15 17:37:49 +0100

    Fix pause and add local highscores (not yet ui-accessible)

---
+++ Client/Assets/HighScores/HighScores.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HighScores : MonoBehaviour {
+
+	public void backToMain() {
+		SceneManager.LoadScene("MainMenu");
+		//SceneManager.SetActiveScene(SceneManager.GetSceneByName(gameScene));
+		//SceneManager.LoadScene(gameScene);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	private void Update() {
+	}
+
+}
