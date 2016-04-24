4751256f7d20f0538598ea3849f1bb4a5bc0975f
georgefst <georgefsthomas@gmail.com>
2016-04-24 17:24:28 +0100

    Add high scores menu

--- Client/Assets/HighScores/HighScores.cs
+++ Client/Assets/HighScores/HighScores.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HighScores : MonoBehaviour {
+
+	public Text text;
 
 	public void backToMain() {
 		SceneManager.LoadScene("MainMenu");
@@ -12,7 +15,12 @@ public class HighScores : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		string scores = "High Scores:\n";
+		for (int i = 0; i<5; i++){
+			int score = PlayerPrefs.GetInt("score" + i.ToString());
+			if (score != 0) scores += score.ToString() + "\n";
+		}
+		text.text = scores;
 	}
 
 	private void Update() {
