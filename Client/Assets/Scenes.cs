83a293a277ce90ba928cc6a635389e8a7ba5bc3b
georgefst <georgefsthomas@gmail.com>
2016-04-28 17:55:26 +0100

    Fix scaling (a bit more)

--- Client/Assets/Scenes.cs
+++ Client/Assets/Scenes.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour {
 
+	private bool paused = false;
+
 	public void ReloadScene() {
 		SceneManager.LoadScene("Test_Scene");
 	}
@@ -23,12 +25,17 @@ public class Scenes : MonoBehaviour {
 			Time.timeScale = 1.0f;
 			ToMainMenu();
 		}
+
 		// pause
 		if (Input.GetKeyDown("escape")) {
-    		if (Time.timeScale == 1.0)
-        		Time.timeScale = 0.0f;
-    		else
+    		if (paused) {
         		Time.timeScale = 1.0f;
+				paused = false;
+			}
+    		else if (Time.timeScale == 1) {
+        		Time.timeScale = 0.0f;
+				paused = true;
+			}
 		}
 	}
 }
