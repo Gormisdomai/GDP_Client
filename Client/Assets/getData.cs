b508d85adb66c4b996f1a02dbdccc686fca51ec8
Mahmoud Ghanem <mahmoudmghanem@Moustafas-MacBook-Pro.local>
2016-05-10 13:30:45 +0100

    changed format to score:user

--- Client/Assets/getData.cs
+++ Client/Assets/getData.cs
@@ -156,7 +156,7 @@ public class getData : MonoBehaviour
 
 	public void Score(String nickname, float s){
 		if (s != 0) {
-			writeSocket ("score:name:"+nickname + s.ToString ());
+			writeSocket ("score:user:"+nickname + s.ToString ());
 		}
 	}
 
